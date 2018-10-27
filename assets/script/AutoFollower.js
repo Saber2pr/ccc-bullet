e8f37698b0ae98aec95b2e823d7bd84e92a92f82
saber2pr <saber2pr@gmail.com>
2018-10-27 17:42:02 +0800

    min

---
+++ assets/script/AutoFollower.js
@@ -0,0 +1,46 @@
+/*
+ * @Author: AK-12 
+ * @Date: 2018-10-27 17:24:50 
+ * @Last Modified by: AK-12
+ * @Last Modified time: 2018-10-27 17:36:06
+ */
+let MathVec = {
+  getFront(origin, target) {
+    let front = cc.v2(target.x - origin.x, target.y - origin.y)
+    return cc.v2(front.x / front.mag(), front.y / front.mag())
+  },
+}
+
+let AnimationMediator = {
+  moveFollowTarget(node, target, speed) {
+    let _speed = typeof (speed) === 'undefined' ? 1 : speed
+    let front = MathVec.getFront(node, target)
+    node.setPosition(node.x + front.x * _speed, node.y + front.y * _speed)
+  }
+}
+
+cc.Class({
+  extends: cc.Component,
+
+  properties: {
+    target: {
+      type: cc.Node,
+      default: null,
+      displayName: '目标靶节点',
+      tooltip: '追踪的目标'
+    },
+    speed: {
+      type: cc.Integer,
+      default: 1,
+      displayName: '移动速度',
+      slide: true,
+      min: 0,
+      max: 10,
+      step: 1,
+    }
+  },
+
+  update() {
+    AnimationMediator.moveFollowTarget(this.node, this.target, this.speed / 2)
+  }
+});
--- assets/script/MoveCtrllor.js
+++ assets/script/MoveCtrllor.js
@@ -17,18 +17,6 @@ let MathVec = {
     return this.getLength(commitPos) < limitRadius ? commitPos : this.getPos(this.getAngle(commitPos), limitRadius)
   },
   /**
-   * 给定点和宽高，限制点的矩形活动范围
-   */
-  limitToRect: function (commitPos, width, height) {
-    return cc.v2(this.limitToWall(commitPos.x, width), this.limitToWall(commitPos.y, height))
-  },
-  /**
-   * 限定值大小
-   */
-  limitToWall: function (value, length) {
-    return Math.abs(value) < length ? value : (value > 0 ? length : -length)
-  },
-  /**
    * 给定点获取其极角(弧度)
    */
   getAngle: function (Pos) {
@@ -37,18 +25,6 @@ let MathVec = {
     } else {
       return -Math.acos(Pos.x / Pos.mag())
     }
-  },
-  /**
-   * 弧度转角度
-   */
-  transformAngle(angle) {
-    return angle * 180 / Math.PI
-  },
-  /**
-   * 角度转弧度
-   */
-  transformAngleToRad(angle) {
-    return angle / (180 * Math.PI)
   },
   /**
    * 给定极角，极径获取点
@@ -61,82 +37,8 @@ let MathVec = {
    */
   getLength: function (commitPos) {
     return commitPos.mag()
-  },
-  /**
-   * 获取随机x或y点(pos, method)
-   */
-  getRandNum: function (pos, method) {
-    return method === 'x' ? cc.v2(pos.x * cc.random0To1(), pos.y) : cc.v2(pos.x, pos.y * cc.random0To1())
-  },
-  /**
-   * 获取随机点(scalePos, minScale)
-   */
-  getRandPos: function (scalePos, minScale) {
-    return cc.v2(scalePos.x * cc.random0To1() + minScale.x, scalePos.y * cc.random0To1() + minScale.y)
-  },
-  /**
-   * 返回origin指向靶目标的单位向量(origin, target)
-   */
-  getFront(origin, target) {
-    var front = cc.v2(target.x - origin.x, target.y - origin.y)
-    return cc.v2(front.x / front.mag(), front.y / front.mag())
-  },
-  /**
-   * 返回两点间距离(origin, target)
-   */
-  getDistance(origin, target) {
-    return cc.v2(target.x - origin.x, target.y - origin.y).mag()
-  },
-  /**
-   * 求子节点的世界坐标(如果父节点在世界下)
-   */
-  transformToParentWorld(childNode) {
-    return cc.v2(childNode.parent.x + childNode.x, childNode.parent.y + childNode.y)
-  },
-  /**
-   * 求数组中最大值
-   */
-  getMaxFromVector(vector) {
-    return Math.max.apply(null, vector)
-  },
-  /**
-   * 求数组中最小值
-   */
-  getMinFromVector(vector) {
-    return Math.min.apply(null, vector)
-  },
-  /**
-   * 求数组中指定元素下标
-   */
-  getOrderFromVector(elem, vector) {
-    for (var order = 0; order < vector.length; order++) {
-      if (elem === vector[order]) {
-        return order
-      }
-    }
-  },
-  /**
-   * 返回负坐标
-   */
-  getPosNegative(Pos) {
-    return cc.v2(-Pos.x, -Pos.y)
-  },
-  /**
-   * 两点相加
-   */
-  addTwoPos(pos1, pos2, method) {
-    return cc.p(pos1.x + pos2.x, pos1.y + pos2.y)
-  },
-  /**
-   * 获取数组中随机元素
-   */
-  getRandElem(vector) {
-    return vector[parseInt((vector.length) * cc.random0To1())]
   }
 }
-
-module.exports = MathVec
-
 /**
  * #### 2018年9月1日15:26:16
  * * 使用它创建轮盘很简单
@@ -201,12 +103,6 @@ let MoveCtrllor = {
     return this.angle
   },
   /**
-   * 获取终止角度(弧度)
-   */
-  getLastAngle() {
-    return this.lastAngle
-  },
-  /**
    * 获取拖动力度
    */
   getForce() {
@@ -239,7 +135,6 @@ let MoveCtrllor = {
     }
   }
 }
-module.exports = MoveCtrllor
 
 const strageyType = cc.Enum({
   CHARACTER: 1001,

4ea2d2ab24dd769bdb7b47290b5589741f01341d
saber2pr <saber2pr@gmail.com>
2018-10-28 21:58:38 +0800

    update

--- assets/script/SceneGo.js
+++ assets/script/SceneGo.js
@@ -2,8 +2,21 @@
  * @Author: AK-12 
  * @Date: 2018-10-26 09:26:44 
  * @Last Modified by: AK-12
- * @Last Modified time: 2018-10-28 10:28:46
+ * @Last Modified time: 2018-10-28 21:52:46
  */
+let AnimationMediator = {
+  TransitionMoveInL(duration, scene1, scene2) {
+    let action = cc.moveBy(duration, cc.winSize.width, 0)
+    scene1.runAction(action.easing(cc.easeSineInOut(duration)))
+    setTimeout(() => {
+      cc.director.loadScene(scene2)
+    }, duration * 1000)
+  }
+}
+const transformType = cc.Enum({
+  common: 0,
+  TransitionMoveInL: 1
+})
 cc.Class({
   extends: cc.Component,
 
@@ -18,13 +31,28 @@ cc.Class({
       type: cc.SceneAsset,
       displayName: '目标场景',
       tooltip: '要切换到的场景'
+    },
+    transform: {
+      type: cc.Enum(transformType),
+      default: transformType.common,
+      displayName: "场景切换效果"
     }
   },
 
   onLoad() {
     this.button.node.on('click', () => {
       // 压缩混淆是否影响_name的值?
-      cc.director.loadScene(this.target._name)
+      // cc.director.loadScene(this.target._name)
+      switch (this.transform) {
+        case transformType.common:
+          cc.director.loadScene(this.target._name)
+          break
+        case transformType.TransitionMoveInL:
+          AnimationMediator.TransitionMoveInL(1, this.node, this.target._name)
+          break
+        default:
+          throw (new Error('scene transform type error'))
+      }
     })
   }
 
